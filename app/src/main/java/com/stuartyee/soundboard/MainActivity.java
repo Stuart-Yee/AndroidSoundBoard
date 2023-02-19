9639a903f8fbd9f1a71f1ab8a4c3daf3c6a2f78d
Stuart-Yee <ki6fzb@gmail.com>
2023-02-18 21:58:59 -0800

    added comments to xml and MainActivity code

--- app/src/main/java/com/stuartyee/soundboard/MainActivity.java
+++ app/src/main/java/com/stuartyee/soundboard/MainActivity.java
@@ -15,17 +15,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    // Method targeted by each button's android:onClick attribute
     public void buttonClick (View view) {
+        // view item is passed in and cast as a Button object
         Button button = (Button) view;
 
+        // Create MediaPlayer object to play .mp3
+        // .mp3 must have same name as the tag in the button
         MediaPlayer mediaPlayer = MediaPlayer.create(
                 this,
+                // Method chain to identify .mp3 based on android:tag in button
                 getResources().getIdentifier(
                         button.getTag().toString(),
                         "raw",
                         getPackageName()
                 )
         );
+        // play the mp3
         mediaPlayer.start();
 
     }
